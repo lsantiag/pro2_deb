071713303cc34ded6a6aff445fc96bd71d2d1262
baba256 <yadneshsamant@gmail.com>
2016-04-23 16:37:27 -0700

    Rojobot picoblaze interface
    Rojobot picoblaze

---
+++ Project2/project_2/project_2.srcs/sources_1/imports/hdl_part1/nexys4_bit_if.v
@@ -0,0 +1,176 @@
+/*
+///////////////////////////////////////////////////////////////////
+//	nexys4_bit_if.v 
+//	Created By:			Yadnesh Samant & Luis Santiago
+//	Last Modified:		23-April-2016 
+//	
+//	Revision History:
+//	-----------------
+//	April		YS,LS		Created this module
+//	
+//	Description
+//	-----------
+//
+///////////////////////////////////////////////////////////////////
+*/
+module nexys4_bot_if#(
+parameter integer RESET_POLARITY_LOW = 1
+)
+(
+  //IO definition for BOT interface
+  input        [7:0] LocX_reg,                   // X-coordinate of rojobot's location
+                     LocY_reg,                   // Y-coordinate of rojobot's location
+                     BotInfo_reg,                // Information about rojobot's activity
+                     Sensors_reg,                // Sensor readings
+                     LMDist_reg,                 // left motor distance register
+                     RMDist_reg,                 // right motor distance register
+
+  input              upd_sysregs,                // flag from PicoBlaze to indicate that the system registers
+
+  output  reg  [7:0] MotCtl,                     // Motor control output
+
+  //IO definition for picoblaze (Names with respct to picoblaze)
+  input        [7:0] port_id,                    // I/O port address
+  input        [7:0] io_dataIn,                  // Data from the picoblaze to be written to I/O register
+  output  reg  [7:0] io_dataOut,                 // Data from the I/O register to the Picoblaze
+  input              write_strobe,               // Asserted to write the data register
+  input              read_strobe,                // Asserted to read the data register
+  output  reg        interrupt,                  // Interrupt request to Picoblaze
+  input              interrupt_ack,              // Interrupt acknowledge from Picoblaze
+
+  //IO definition with debounce module
+  input      [15:0]  db_sw,                     // slider switches
+  input       [5:0]  db_btns,                   // pushbutton inputs - including CPU RESET button
+
+  //IO definition with 7-segment display
+  output  reg  [4:0] dig7,dig6,
+                     dig5,dig4,
+                     dig3,dig2,
+                     dig1,dig0,
+
+  output  reg  [7:0] dp,
+  output  reg [15:0] led,
+
+  //Global System interface
+  input sysclk,
+  input sysrst
+  );
+
+  //System Asserted RESET
+  wire reset_in = RESET_POLARITY_LOW ? ~sysrst : sysrst;
+
+  /*================================================================*/
+  //                    General Input port mapping                  //
+  /*================================================================*/
+
+  always @(posedge sysclk) begin
+    if(reset_in)
+            io_dataOut<=8'h0;
+    else begin
+            case (port_id)
+                //Read the
+                8'h0A:
+                    io_dataOut<=LocX_reg;
+                8'h0B:
+                    io_dataOut<=LocY_reg;
+                8'h0C:
+                    io_dataOut<=BotInfo_reg;
+                8'h0D:
+                    io_dataOut<=Sensors_reg;
+                8'h0E:
+                    io_dataOut<=LMDist_reg;
+                8'h0F:
+                    io_dataOut<=RMDist_reg;
+
+                //Read the higher switch status
+                8'h11:
+                    io_dataOut<=db_sw[15:8];
+                //Read the lower switch status
+                8'h01:
+                    io_dataOut<=db_sw[7:0];
+                // Read the status of debounce buttons
+                8'h00:
+                    io_dataOut<={3'b000,db_btns};
+                default:
+                    io_dataOut<=8'bxxxxxxxx;
+            endcase
+        end
+  end
+  
+  /*===================================================================*/
+  //                       General Output port Mapping                 //
+  /*===================================================================*/
+  always @(posedge sysclk) begin
+    if(reset_in) begin
+        //Resetting all the inputs to IO
+        led <= 16'b0;
+        dig0 <= 5'b0;
+        dig1 <= 5'b0;
+        dig2 <= 5'b0;
+        dig3 <= 5'b0;
+        dig4 <= 5'b0;
+        dig5 <= 5'b0;
+        dig6 <= 5'b0;
+        dig7 <= 5'b0;
+        MotCtl <= MotCtl;
+    end            
+    else begin
+        if(write_strobe == 1'b1) begin      //Validating the write signal
+            case (port_id)
+                8'h02:
+                    led[7:0] <= io_dataIn;
+                8'h12:
+                    led[15:8] <= io_dataIn;
+                8'h13:
+                    dig7 <= io_dataIn;
+                8'h14:
+                    dig6 <= io_dataIn;
+                8'h15:
+                    dig5 <= io_dataIn;
+                8'h16:
+                    dig4 <= io_dataIn;
+                8'h17:
+                    dp[7:4] <= io_dataIn[7:4];
+                8'h03:
+                    dig3 <= io_dataIn;
+                8'h04:
+                    dig2 <= io_dataIn;
+                8'h05:
+                    dig1 <= io_dataIn;
+                8'h06:
+                    dig0 <= io_dataIn;
+                8'h07:
+                    dp[3:0] <= io_dataIn[3:0];
+                8'h09:
+                    MotCtl <= io_dataIn;
+                default:
+                    begin
+                        led <= led;
+                        dig0 <= dig0;
+                        dig1 <= dig1;
+                        dig2 <= dig2;
+                        dig3 <= dig3;
+                        dig4 <= dig4;
+                        dig5 <= dig5;
+                        dig6 <= dig6;
+                        dig7 <= dig7;
+                        MotCtl <= MotCtl;
+                    end
+            endcase
+        end
+    end
+ end
+  /*=========================================================================*/
+  //                    Interrupt Logic for Interface                        //
+  /*=========================================================================*/
+  always @(posedge sysclk) begin
+    if(reset_in)
+        interrupt<=1'b0;            //Interrupt Disabled for reset condition
+    else if(interrupt_ack ==1'b1) 
+        interrupt <= 1'b0;          //Interrupt Disabled, once it is acknowledged
+    else if(upd_sysregs)
+        interrupt <= 1'b1;          //Interrupt enabled due to request from botsim
+    else
+        interrupt <= interrupt;     //default 
+  end
+ endmodule
---
+++ Project2/project_2/project_2.srcs/sources_1/imports/hdl_part1/nexys4fpga.v
@@ -0,0 +1,232 @@
+module Nexys4fpga ( 
+ 
+input           clk,        // 100MHz clock from on-board oscillator  
+input           btnL, btnR, // pushbutton inputs - left 
+                            //(db_btns[4])and right (db_btns[2])  
+input           btnU, btnD, // pushbutton inputs - up (db_btns[3])            
+                            // and down (db_btns[1])  
+input           btnC,       // pushbutton inputs - center button ->            
+                            // db_btns[5]  
+input           btnCpuReset,// red pushbutton input -> db_btns[0] 
+input 	[9:0]   vid_row,    // video logic row address
+                vid_col,    // video logic column address
+input   [15:0]  sw,         // switch inputs
+output  [1:0]   vid_pixel_out,  // pixel (location) value    
+output  [15:0]  led,        // LED outputs   
+output  [6:0]   seg,        // Seven segment display cathode pins  
+output          dp,  
+output  [7:0]   an,         // Seven segment display anode pins     
+output  [7:0]   JA          // JA Header 
+);
+
+
+
+
+// parameter  
+parameter SIMULATE = 0;  
+ // internal variables  
+wire  [15:0]    db_sw;          // debounced switches 
+wire  [5:0]     db_btns;        // debounced buttons
+wire            sysclk;         // 100MHz clock from on-board 
+// oscillator   
+wire            sysreset;       // system reset signal – asserted 
+ // high to force reset    
+wire  [4:0]     dig7, dig6, dig5, dig4, // display digits 7-4
+                dig3, dig2, dig1, dig0; // display digits 3-0  
+wire  [7:0]     decpts;         // decimal points  
+wire [7:0]      segs_int;       // segment outputs (internal)  
+wire [63:0]     digits_out;     // digits_out (only for simulation)  
+wire [17:0]     instruction; 
+
+ 
+ // PicoBlaze interface  
+wire [11:0] address;  
+wire        bram_enable;  
+wire [7:0]  port_id; 
+wire [7:0]  out_port;  
+wire [7:0]  in_port;  
+wire        write_strobe;  
+wire        k_write_strobe;  
+wire        read_strobe; 
+wire        interrupt;  
+wire        interrupt_ack;  
+wire        sleep;          //kcpsm6_sleep;  
+wire        kcpsm6_reset;  
+wire        cpu_reset;  
+wire        rdl;  
+wire        int_request;
+
+// new block (only for reference remove later )
+wire [7:0]  motctl;
+wire [7:0]  locX, locY;
+wire [7:0]  botinfo;
+wire [7:0]  sensors;
+wire [7:0]  lmdist;
+wire [7:0]  rmdist;
+wire        upd_sysregs;
+
+/*
+// set up the display by blanking all but Digit[0] 
+assign  dig7 = {5'b00011};  
+assign  dig6 = {5'b11111};  
+assign  dig5 = {5'b11111};  
+assign  dig4 = {5'b11111};  
+assign  dig3 = {5'b11111};  
+assign  dig2 = {5'b11111};  
+assign  dig1 = {5'b11111}; 
+*/
+
+/*
+ // The debounced switches, 7-segment Digit[0] and the 7-segment decimal   
+ // points are writable by the Picoblaze  assign    
+ dig0 = digit0_int[4:0];    
+*/
+
+ // global assigns  
+assign  sysclk      =   clk;  
+assign  sysreset    =   ~db_btns[0]; // btnCpuReset is asserted low so invert it  
+assign  sw_high     =   db_sw[15:8];  
+assign  sw_low      =   db_sw[7:0]; 
+//assign  led         =   {leds_high, leds_low};  
+assign  dp          =   segs_int[7];  
+assign  seg          =   segs_int[6:0];
+assign  JA          =   {sysclk, sysreset, 6'b000000};
+
+
+// instantiate the debounce module
+// RESET_POLARITLY_LOW is 1 because btnCpuReset is asserted  
+// high and the debounced version of btnCpuReset becomees  
+// sysreset  
+debounce    #(
+                .RESET_POLARITY_LOW(1),
+                .SIMULATE(SIMULATE)  )   
+        DB  (
+                .clk(sysclk),
+                .pbtn_in({btnC,btnL,btnU,btnR,btnD,btnCpuReset}),
+                .switch_in(sw),   
+                .pbtn_db(db_btns),   
+                .swtch_db(db_sw)  
+            );
+                
+                
+// instantiate the 7-segment, 8-digit display  
+sevensegment #(   
+                .RESET_POLARITY_LOW(1), 
+                .SIMULATE(SIMULATE)  ) 
+         SSB    (   // inputs for control signals   
+                 .d0(dig0),   
+                 .d1(dig1),    
+                 .d2(dig2),   
+                 .d3(dig3),   
+                 .d4(dig4),   
+                 .d5(dig5),   
+                 .d6(dig6),   
+                 .d7(dig7),   
+                 .dp(decpts),      
+                 
+        // outputs to seven segment display   
+        .seg(segs_int),      
+        .an(an),      
+        // clock and reset signals (100 MHz clock, active high reset)   
+        .clk(sysclk),   
+        .reset(sysreset),      
+        
+        // ouput for simulation only   
+        .digits_out(digits_out)  );
+        
+        
+        
+        
+// instantiate the PicoBlaze and instruction ROM 
+ assign kcpsm6_sleep = 1'b0;  
+ assign kcpsm6_reset = sysreset | rdl;
+
+kcpsm6  #(   
+            .interrupt_vector (12'h3FF),   
+            .scratch_pad_memory_size(64),   
+            .hwbuild  (8'h00))   
+    APPCPU(   
+            .address   (address),   
+            .instruction  (instruction),   
+            .bram_enable  (bram_enable),   
+            .port_id   (port_id),   
+            .write_strobe  (write_strobe),   
+            .k_write_strobe (k_write_strobe),   
+            .out_port   (out_port),   
+            .read_strobe  (read_strobe),   
+            .in_port   (in_port),   
+            .interrupt   (interrupt),   
+            .interrupt_ack  (interrupt_ack),   
+            .reset    (kcpsm6_reset),   
+            .sleep   (kcpsm6_sleep),   
+            .clk    (sysclk));  
+       
+ proj2demo #(  .C_FAMILY     ("7S"),      //Family 'S6' or 'V6' or '7S'   
+             .C_RAM_SIZE_KWORDS (2),    //Program size '1', '2' or '4'   
+             .C_JTAG_LOADER_ENABLE (1)) //Include JTAG Loader when set to 1'b1   
+   PRJ_Demo (                   
+            .rdl    (),   
+            .enable   (bram_enable),   
+            .address   (address),   
+            .instruction  (instruction),   
+            .clk    (sysclk));        
+          
+
+bot ROJO(
+    .clk(clk),
+    .reset(sysreset),
+    .MotCtl_in(motctl),
+    .LocX_reg(locX),		// X-coordinate of rojobot's location		
+    .LocY_reg(locY),        // Y-coordinate of rojobot's location
+    .Sensors_reg(sensors),    // Sensor readings
+    .BotInfo_reg(botinfo),    // Information about rojobot's activity
+    .LMDist_reg(lmdist),        // left motor distance register
+    .RMDist_reg(rmdist),
+    .upd_sysregs(upd_sysregs),
+    .vid_row(),
+    .vid_col(),
+    .vid_pixel_out()
+);
+// instantiate the PicoBlaze I/O register interface  
+  
+nexys4_bot_if #(
+                .RESET_POLARITY_LOW(1)
+                )  
+    N4IF(
+            .write_strobe(write_strobe),   
+            .read_strobe(read_strobe),   
+            .port_id(port_id),   
+            .io_dataIn(out_port),      // data from Picoblaze to the I/O register   
+            .io_dataOut(in_port),    // data from I/O register to Picoblaze      
+            .interrupt_ack(interrupt_ack),   
+            .interrupt(interrupt),   
+            
+            .LocX_reg(locX),
+            .LocY_reg(locY),
+            .BotInfo_reg(botinfo),
+            .Sensors_reg(sensors),
+            .LMDist_reg(lmdist),
+            .RMDist_reg(rmdist),
+            .upd_sysregs(upd_sysregs),
+            .MotCtl(motctl),
+            
+            .db_sw(db_sw),
+            .db_btns(db_btns),
+            .dig7(dig7),
+            .dig6(dig6), 
+            .dig5(dig5),
+            .dig4(dig4),
+            .dig3(dig3),
+            .dig2(dig2),
+            .dig1(dig1),
+            .dig0(dig0),
+            .dp(decpts),
+            .led(led),
+            
+            .sysclk(sysclk),   
+            .sysrst(sysreset) 
+            );     
+
+endmodule 
+        
+        
